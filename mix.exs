0e7660b26f9f51e89ac757fd904e0e8837a6e06b
Ryan Buckley <arebuckley@gmail.com>
2021-09-28 13:22:02 -0700

    Update deps

--- mix.exs
+++ mix.exs
@@ -33,7 +33,7 @@ defmodule Lava.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.6.0-rc.1", override: true},
+      {:phoenix, "~> 1.6.0", override: true},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},
