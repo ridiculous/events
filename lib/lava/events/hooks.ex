8c5c648c9cf88d01bcc89e96fcb97143b27e0bff
Ryan Buckley <arebuckley@gmail.com>
2021-11-10 15:47:54 -0800

    Fix typespec

--- lib/lava/events/hooks.ex
+++ lib/lava/events/hooks.ex
@@ -1,7 +1,7 @@
 defprotocol Lava.Events.Hooks do
   @fallback_to_any true
   @type event :: Lava.Events.Event
-  @spec event_created(map, event) :: [{atom, event}]
+  @spec event_created(map, event) :: {atom, event}
   def event_created(attrs, event)
 end
 
