cf704d2a4259be77e187ae85f0d1a00e66cf6802
Ryan Buckley <ryan@mauisdc.com>
2021-12-05 18:13:50 -1000

    Update top level events for explorer and timelines

--- lib/lava/events.ex
+++ lib/lava/events.ex
@@ -2,13 +2,13 @@ defmodule Lava.Events do
   use Lava.Events.Core
 
   def by_timeline(timeline) do
-    Repo.all(from e in Event, where: e.timeline_id == ^timeline.id, order_by: e.inserted_at)
+    Repo.all(from e in Event, where: e.timeline_id == ^timeline.id and is_nil(e.source_event_id), order_by: e.inserted_at)
   end
 
   def get_event!(id), do: Repo.get!(Event, id)
 
   def source_events do
-    Repo.all(from e in Event, where: is_nil(e.source_event_id), order_by: e.id)
+    Repo.all(from e in Event, where: is_nil(e.source_event_id) and is_nil(e.timeline_id), order_by: e.id)
   end
 
   def change_event(%Event{} = event, attrs \\ %{}) do
